41e725bb85d36bf263dc96feb60489a9f87cdb1c
Braian <braian.bergmnn@gmail.com>
2021-04-28 17:31:17 +0300

    Made codefirst migratsion and added erd

---
+++ TARge20/TARge20.Core/Domain/AccessLevel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge20.Core.Domain
+{
+    public  class AccessLevel
+    {
+        public Guid Id { get; set; }
+        public enum AccessType
+        {
+            low,
+            medium,
+            high,
+            special
+
+        }
+    }
+}
